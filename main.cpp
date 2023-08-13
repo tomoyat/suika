2e515770e318caa04ecfb7d6d0ad1e7e9b62b03a
tomoyat <1283480+tomoyat@users.noreply.github.com>
2023-08-13 21:05:25 +0900

    mainを変更

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include "logger.h"
+#include "util.h"
+#include "EtherDevice.h"
+
+constexpr char tun_device[] = "/dev/net/tun";
+
+constexpr char user[] = "root";
+constexpr char tun_device_name[] = "suika_device";
+constexpr char tun_ip_range[] = "192.0.2.1/24";
+
+void setupTunDevice() {
+    suika::util::exec(std::format("ip tuntap add mode tap user {} name {}", user, tun_device_name));
+    suika::util::exec(std::format("ip addr add {} dev {}", tun_ip_range, tun_device_name));
+    suika::util::exec(std::format("ip link set {} up", tun_device_name));
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    setupTunDevice();
+
+    auto etherDevice = suika::device::EtherDevice(std::string{tun_device}, std::string{tun_device_name});
+    etherDevice.open();
+
     return 0;
 }
