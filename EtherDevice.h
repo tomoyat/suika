4a2ad61ae4036590014ca9182f242e2b21dd6f8a
tomoyat <1283480+tomoyat@users.noreply.github.com>
2023-09-03 17:42:10 +0900

    file名を変更

--- EtherDevice.h
+++ EtherDevice.h
@@ -8,37 +8,7 @@
 #include <array>
 
 #include "NetworkInterface.h"
-
-namespace suika::device {
-    struct Device {
-        std::vector<std::shared_ptr<suika::network::NetworkInterface>> networkInterfaces;
-
-        virtual int open() = 0;
-
-        virtual int handler(pthread_t) = 0;
-
-        virtual int getIrq() = 0;
-
-        virtual std::string getInfo() = 0;
-
-        virtual int addNetworkInterface(std::shared_ptr<suika::network::NetworkInterface> interfacePtr) = 0;
-
-        virtual void setSelfPtr(std::shared_ptr<suika::device::Device> selfPtr_) = 0;
-
-        virtual std::shared_ptr<suika::device::Device> getSelfPtr() = 0;
-
-        virtual ~Device() = default;
-
-        std::shared_ptr<suika::network::NetworkInterface> getTargetInterface(int family) {
-            for (auto &interface: networkInterfaces) {
-                if (interface->family == family) {
-                    return interface;
-                }
-            }
-            throw std::runtime_error("interface not found");
-        }
-    };
-};
+#include "Device.h"
 
 namespace suika::ether {
     constexpr int ETHER_ADDR_LEN = 6;
